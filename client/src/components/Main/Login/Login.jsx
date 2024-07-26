b7e3e9cea14ae01487f44637107c227ca458db32
Miguel Pardal <miguelpardalesparis@gmail.com>
2024-07-26 12:30:27 +0200

    cors setup in index.js

--- client/src/components/Main/Login/Login.jsx
+++ client/src/components/Main/Login/Login.jsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+axios.defaults.withCredentials = true;
 
 const Login = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  // const URL = 'https://dt-empieza-por-educar.onrender.com';
+  const URL = import.meta.env.VITE_API_URL;
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(`http://localhost:3000/api/empleados/login`, {
+        email: email,
+        password: password
+      });
+
+      console.log(response);
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <section className="login">
       <img 
@@ -10,18 +35,18 @@ const Login = () => {
         className="home-logo"
       />
       <h2>Login</h2>
-      <form /* onSubmit={handleSubmit} */ >
+      <form  onSubmit={handleSubmit}  >
         <input
           type="email"
-          // value={email}
-          // onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           placeholder="Email"
           required
         />
         <input
           type="password"
-          // value={password}
-          // onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           placeholder="Password"
           required
         />
