c3201d42c2d64393948d3870187383844821290d
Nitolez <antonioglezt98@gmail.com>
2024-07-27 22:35:36 +0200

    Correccion de estilos candidatos

--- client/src/components/Main/Candidatos/ListaCandidatos/TarjetaCandidatos/TarjetaCandidatos.jsx
+++ client/src/components/Main/Candidatos/ListaCandidatos/TarjetaCandidatos/TarjetaCandidatos.jsx
@@ -5,30 +5,19 @@ const TarjetaCandidatos = ({ candidatos }) => {
     <>
       <article className="tarjetaCandidatos">
         <div className="nombre">
-          <h3>Nombre:</h3>
-          <p>{candidatos.nombre_candidato} {candidatos.apellidos_candidato}</p>
+          <h3>{candidatos.nombre_candidato} {candidatos.apellidos_candidato}</h3>
         </div>
-        <div className="email">
+        <div className="otros">
           <h3>Email:</h3>
           <p>{candidatos.email_candidato}</p>
-        </div>
-        <div className="telefono">
           <h3>Teléfono:</h3>
           <p>{candidatos.telefono_candidato}</p>
-        </div>
-        <div className="edad">
           <h3>Edad:</h3>
           <p>{candidatos.edad}</p>
-        </div>
-        <div className="carrera">
           <h3>Carrera:</h3>
           <p>{candidatos.carrera}</p>
-        </div>
-        <div className="notaMedia">
           <h3>Nota Media:</h3>
           <p>{candidatos.nota_media}</p>
-        </div>
-        <div className="nivelIngles">
           <h3>Nivel de Inglés:</h3>
           <p>{candidatos.nivel_ingles}</p>
         </div>
