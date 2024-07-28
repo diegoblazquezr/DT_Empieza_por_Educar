83e95e727a9e854750821867622d9a765a22b439
SergioLM7 <sergiolillom@gmail.com>
2024-07-28 12:24:16 +0200

    CORS fixed

--- index.js
+++ index.js
@@ -21,8 +21,20 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://dt-form-registro-exe.onrender.com'
+  ];
+
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: (origin, callback) => {
+        if (/*!origin ||*/ allowedOrigins.indexOf(origin) !== -1) {
+          callback(null, true);
+        } else {
+          callback(new Error('No permitido por CORS'));
+        }
+      },
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
