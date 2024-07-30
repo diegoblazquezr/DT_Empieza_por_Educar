35714c77247009995178adad6e2b6f7a06cd9b1f
diegoblazquezr <diegoblazquezr@gmail.com>
2024-07-30 18:48:05 +0200

    fix indexjs

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const app = express(); // Initialize server
-const port = /*process.env.PORT ||*/ 3000;
-const URL = /*process.env.API_URL ||*/ 'http://localhost';
+const port = process.env.PORT || 3000;
+const URL = process.env.API_URL || 'http://localhost';
 // const swaggerUi = require('swagger-ui-express');
 // const swaggerDocument = require('./swagger.json');
 
@@ -24,7 +25,10 @@ app.use(cookieParser());
 
 const allowedOrigins = [
     'http://localhost:5173',
-    'https://dt-form-registro-exe.onrender.com'
+    'http://localhost:3000',
+    'https://dt-form-registro-exe.onrender.com',
+    'https://dt-empieza-por-educar.onrender.com',
+    'https://api-empleados-2nuf.onrender.com'
   ];
 
 app.use(cors({
@@ -38,16 +42,6 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
-// app.use(cors({
-//     origin: function (origin, callback) {
-//       if (origin === 'http://localhost:5173' || origin === '') {
-//         callback(null, true);
-//       } else {
-//         callback(new Error('Not allowed by CORS'));
-//       }
-//     },
-//     credentials: true
-//   }));
 
 // Routes
 const candidaturasRoutes = require("./routes/candidaturas.routes");
@@ -56,7 +50,6 @@ const candidatosRoutes = require ("./routes/candidatos.routes");
 const nodemailerRoutes = require ("./routes/nodemailer.routes");
 const competenciasRoutes = require ("./routes/competencias.routes");
 
-
 // API Routes
 app.use('/api/candidaturas', candidaturasRoutes);
 app.use('/api/empleados', empleadosRoutes);
@@ -64,10 +57,9 @@ app.use('/api/candidatos', candidatosRoutes);
 app.use('/api/mailing', nodemailerRoutes);
 app.use('/api/competencias', competenciasRoutes);
 
-
-/*app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});*/
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/dist/index.html'));
+});
 
 const server = app.listen(port, () => {
     console.log(`App listening on ${URL}:${port}`);
