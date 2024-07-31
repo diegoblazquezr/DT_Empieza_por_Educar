3f57738101e8d9a40f5bfc958ae3703063569da3
Miguel Pardal <miguelpardalesparis@gmail.com>
2024-07-31 09:55:14 +0200

    buscadorEmpleados fix

--- client/src/components/Main/Empleados/ListaEmpleados/ListaEmpleados.jsx
+++ client/src/components/Main/Empleados/ListaEmpleados/ListaEmpleados.jsx
@@ -2,83 +2,73 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import TarjetaEmpleado from './TarjetaEmpleado';
 import { v4 as uuidv4 } from 'uuid';
+import { ProgressBar } from 'react-loader-spinner';
 
-const ListaEmpleados = ({ empleadoName }) => {
+const ListaEmpleados = ({ empleadoEmail }) => {
   const [empleadoDetails, setEmpleadoDetails] = useState([]);
-  const [filteredEmpleadoDetails, setFilteredEmpleadoDetails] = useState([]);
-  const URL = import.meta.env.VITE_API_DATA || 'http://localhost:3000';
+  const [loading, setLoading] = useState(false);
+  const URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
   const getEmpleadoDetails = async () => {
     try {
-      const response = await axios.get(`${URL}/all_empleados?offset=0&limit=10`);
-      // console.log('Datos obtenidos de la API:', response.data.empleados);
-      return response.data.empleados;
+      setLoading(true);
+      const response = await axios.get(`${URL}/api/empleados?email_empleado=${empleadoEmail}`);
+      console.log(response.data[0]);
+      return response.data[0];
     } catch (error) {
       console.error('Error obteniendo empleados', error);
       return [];
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
-    const fetchEmpleados = async () => {
-      const details = await getEmpleadoDetails();
-      // console.log('Detalles de empleados:', details);
-      setEmpleadoDetails(details);
-      setFilteredEmpleadoDetails(details);
+    const fetchEmpleado = async () => {
+      if (empleadoEmail) {
+        const details = await getEmpleadoDetails(empleadoEmail);
+        setEmpleadoDetails(details);
+      } else {
+        setEmpleadoDetails(null);
+      }
     };
-    fetchEmpleados();
-  }, []);
-
-  useEffect(() => {
-    if (empleadoName) {
-      const filteredDetails = empleadoDetails.filter((empleado) =>
-        empleado.nombre.toLowerCase().includes(empleadoName.toLowerCase())
-      );
-      console.log('Detalles de empleados filtrados:', filteredDetails);
-      setFilteredEmpleadoDetails(filteredDetails);
-    } else {
-      setFilteredEmpleadoDetails(empleadoDetails);
-    }
-  }, [empleadoName, empleadoDetails]);
-
+    fetchEmpleado();
+  }, [empleadoEmail]);
 
   const handleUpdate = (updatedEmpleado) => {
-    setEmpleadoDetails((prevDetails) =>
-      prevDetails.map((empleado) =>
-        empleado.id_empleado === updatedEmpleado.id_empleado ? updatedEmpleado : empleado
-      )
-    );
-    setFilteredEmpleadoDetails((prevDetails) =>
-      prevDetails.map((empleado) =>
-        empleado.id_empleado === updatedEmpleado.id_empleado ? updatedEmpleado : empleado
-      )
-    );
+    if (empleadoDetails && empleadoDetails.id_empleado === updatedEmpleado.id_empleado) {
+      setEmpleadoDetails(updatedEmpleado);
+    }
   };
 
   const handleDelete = (id_empleado) => {
-    setEmpleadoDetails((prevDetails) =>
-      prevDetails.filter((empleado) => empleado.id_empleado !== id_empleado)
-    );
-    setFilteredEmpleadoDetails((prevDetails) =>
-      prevDetails.filter((empleado) => empleado.id_empleado !== id_empleado)
-    );
+    if (empleadoDetails && empleadoDetails.id_empleado === id_empleado) {
+      setEmpleadoDetails(null);
+    }
   };
 
   return (
-    <section className="ListaEmpleados">
-      {Array.isArray(filteredEmpleadoDetails) && filteredEmpleadoDetails.length > 0 ? (
-        filteredEmpleadoDetails.map((empleado) => (
-          <TarjetaEmpleado
-            empleado={empleado}
-            key={uuidv4()}
-            onUpdate={handleUpdate}
-            onDelete={handleDelete}
-          />
-        ))
+    !loading ? <section className="ListaEmpleados">
+      {empleadoDetails ? (
+        <TarjetaEmpleado
+          empleado={empleadoDetails}
+          onUpdate={handleUpdate}
+          onDelete={handleDelete}
+        />
       ) : (
         <p>No se encontraron empleados.</p>
       )}
-    </section>
+    </section> : <ProgressBar
+          visible={true}
+          height="100"
+          width="100"
+          color="#4fa94d"
+          barColor='#FFCC00'
+          borderColor='#11654d'
+          ariaLabel="progress-bar-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          />
   );
 };
 
